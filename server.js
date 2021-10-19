db20f431856e9c0054a6e33d7f312f165cc3ed99
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2021-10-19 11:00:12 -0500

    mysql2 and database connections

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -5,17 +6,23 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'd@s@lliver1106',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
-app.get('/', (req,res) => {
-  res.json({
-    message: 'Hello, World!'
-  });
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
 });
 
 app.use((req, res) => {
   res.status(404).end();
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
